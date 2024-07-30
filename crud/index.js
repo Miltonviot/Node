3fee855c8c5b63138800ffc31c06f83c484bdb97
Milton viot <milton.viot@gmail.com>
2024-07-30 13:22:48 -0300

    primeiro commit

--- crud/index.js
+++ crud/index.js
@@ -1,1 +1,12 @@
-teste
+const express = require("express");
+const app = express();
+
+
+app.set('view engine','ejs'); //Engine que renderiza o HTML
+
+app.get("/", (req, res)=>{ // Rota Principal
+    res.send("Bem vindo!");
+});
+app.listen(88, () => { // porta q o servidor web rodas
+    console.log("O servidor está rodando");
+});
